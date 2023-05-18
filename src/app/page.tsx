941e58dd6b7ef82d59df1ae1030cd3d518a7eb62
Yusuke <yhakamaya@adobe.com>
2023-05-18 19:34:17 +0900

    update Home
    use actual data instead of dummy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
+import { Beverage } from "@/interfaces/beverage";
 import Image from "next/image";
 
-export default function Home() {
-  const products = Array.from({ length: 20 }).map((_, i) => ({
-    id: i + 1,
-    name: `Basic Tee ${i + 1}`,
-    href: "#",
-    imageSrc: `https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-0${
-      (i % 4) + 1
-    }.jpg`,
-    imageAlt: "Alt",
-    price: "$35",
-    color: "Black",
-  }));
+export default async function Home() {
+  const beverages = await fetchBeverages();
 
   return (
     <main className="flex min-h-screen flex-col justify-between">
@@ -21,31 +12,31 @@ export default function Home() {
             ドリンク
           </h2>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id} className="group relative">
+            {beverages.map((beverage) => (
+              <div key={beverage.slug} className="group relative">
                 <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <Image
-                    width={250}
-                    height={250}
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
+                    src={`${process.env.NEXT_PUBLIC_AEM_HOST}${beverage.primaryImage._path}`}
+                    alt={beverage.title}
+                    width={beverage.primaryImage.width}
+                    height={beverage.primaryImage.height}
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
+                      <a href={`/beverages/${beverage.slug}`}>
                         <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
+                        {beverage.title}
                       </a>
                     </h3>
                     <p className="mt-1 text-sm text-gray-500">
-                      {product.color}
+                      {beverage.description.plaintext.slice(0, 50)}
                     </p>
                   </div>
                   <p className="text-sm font-medium text-gray-900">
-                    {product.price}
+                    {beverage.price}
                   </p>
                 </div>
               </div>
@@ -55,4 +46,25 @@ export default function Home() {
       </div>
     </main>
   );
+}
+
+async function fetchBeverages(): Promise<Beverage[]> {
+  const res = await fetch(
+    "https://publish-p111360-e1081459.adobeaemcloud.com/graphql/execute.json/wknd-shared/beverages-all",
+    {
+      next: {
+        revalidate: 60 * 60 * 24, // 24時間キャッシュする
+      },
+    }
+  );
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const json = await res.json();
+  const { data } = json;
+  const beverages = data.beverageList.items;
+
+  console.log(beverages);
+
+  return beverages;
 }
