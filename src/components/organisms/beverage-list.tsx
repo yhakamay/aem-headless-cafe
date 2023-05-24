41a81e1299a34cb369407cb6ad2de1775836ac54
Yusuke <yhakamaya@adobe.com>
2023-05-24 15:04:12 +0900

    fix broken layout

--- src/components/organisms/beverage-list.tsx
+++ src/components/organisms/beverage-list.tsx
@@ -10,25 +10,27 @@ export function BeverageList(props: Props) {
   const { beverages } = props;
 
   return (
-    <div className="mx-auto max-w-2xl px-4 pb-16 sm:px-6 sm:pb-24 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-        Beverages
-      </h2>
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {beverages.map((beverage) => (
-          <div key={beverage.slug}>
-            <FadeIn>
-              <Item
-                imgPath={beverage.primaryImage._path}
-                imgSrcWidth={beverage.primaryImage.width}
-                imgSrcHeight={beverage.primaryImage.height}
-                slug={beverage.slug}
-                title={beverage.title}
-                description={beverage.description.plaintext}
-              />
-            </FadeIn>
-          </div>
-        ))}
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl pb-16 sm:pb-24 lg:max-w-none lg:pb-32">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Beverages
+        </h2>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {beverages.map((beverage) => (
+            <div key={beverage.slug}>
+              <FadeIn>
+                <Item
+                  imgPath={beverage.primaryImage._path}
+                  imgSrcWidth={beverage.primaryImage.width}
+                  imgSrcHeight={beverage.primaryImage.height}
+                  slug={beverage.slug}
+                  title={beverage.title}
+                  description={beverage.description.plaintext}
+                />
+              </FadeIn>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/organisms/food-list.tsx
+++ src/components/organisms/food-list.tsx
@@ -10,23 +10,27 @@ export default function FoodList(props: Props) {
   const { foods } = props;
 
   return (
-    <div className="mx-auto max-w-2xl px-4 pb-16 sm:px-6 sm:pb-24 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900">Foods</h2>
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {foods.map((food) => (
-          <div key={food.slug}>
-            <FadeIn>
-              <Item
-                imgPath={food.primaryImage._path}
-                imgSrcWidth={food.primaryImage.width}
-                imgSrcHeight={food.primaryImage.height}
-                slug={food.slug}
-                title={food.title}
-                description={food.description.plaintext}
-              />
-            </FadeIn>
-          </div>
-        ))}
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl pb-16 sm:pb-24 lg:max-w-none lg:pb-32">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Foods
+        </h2>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {foods.map((food) => (
+            <div key={food.slug}>
+              <FadeIn>
+                <Item
+                  imgPath={food.primaryImage._path}
+                  imgSrcWidth={food.primaryImage.width}
+                  imgSrcHeight={food.primaryImage.height}
+                  slug={food.slug}
+                  title={food.title}
+                  description={food.description.plaintext}
+                />
+              </FadeIn>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
