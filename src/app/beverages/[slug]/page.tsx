361bca43c78db975c20d27a5cd841b5f2bd8fc95
Yusuke <yhakamaya@adobe.com>
2023-05-24 15:41:44 +0900

    extract ItemDetails from /foods/[slug]/page.tsx and beverages/[slug]/page.tsx

--- src/app/beverages/[slug]/page.tsx
+++ src/app/beverages/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import { fetchBeverages } from "@/app/page";
-import Badge from "@/components/atoms/badge";
-import FadeIn from "@/components/atoms/fade-in";
+import ItemDetails from "@/components/molecules/item-details";
 import { Beverage } from "@/interfaces/beverage";
 import { BeverageDetails } from "@/interfaces/beverage-details";
-import Image from "next/image";
 
 export async function generateMetadata({
   params,
@@ -28,30 +26,12 @@ export default async function BeverageDetails({
   const beverage = await fetchBeverageDetails(slug);
 
   return (
-    <div className="bg-white">
-      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-        <div>
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            {beverage.title}
-          </h2>
-          {beverage.flavor && (
-            <div className="pt-3">
-              <Badge>{beverage.flavor.toUpperCase()}</Badge>
-            </div>
-          )}
-          <p className="mt-4 text-gray-500">{beverage.description.plaintext}</p>
-        </div>
-        <FadeIn>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_AEM_HOST}${beverage.primaryImage._path}`}
-            alt={beverage.title}
-            width={beverage.primaryImage.width}
-            height={beverage.primaryImage.height}
-            className="rounded-lg bg-gray-100"
-          />
-        </FadeIn>
-      </div>
-    </div>
+    <ItemDetails
+      title={beverage.title}
+      description={beverage.description.plaintext}
+      label={beverage.flavor}
+      primaryImage={beverage.primaryImage}
+    />
   );
 }
 
